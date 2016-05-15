6370f7d5d502a44d4ef3106f80cfdcaf9dc63b22
Jennifer Blight <jgblight@gmail.com>
2016-05-15 10:40:28 -0700

    uber_link

--- transfers/models.py
+++ transfers/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.http import urlencode
+
+from geopy.geocoders import Nominatim
 
 import json
 from twython import Twython
@@ -31,7 +34,10 @@ class PaymentRequest(models.Model):
 
     @property
     def uber_link(self):
-        return "uber://action=setPickup&client_id=RXjda_RLc1B5KhzoOB9nDnkaGJOkJAmv&dropoff[latitude]={}[longitude]={}&dropoff[nickname]=Your%20Friend&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d".format(self.latitude, self.longitude)
+        #geolocator = Nominatim()
+        #location = geolocator.reverse("{}, {}".format(self.latitude, self.longitude))
+        #formatted_address = urlencode(location)
+        return "uber://action=setPickup&client_id=RXjda_RLc1B5KhzoOB9nDnkaGJOkJAmv&dropoff[latitude]={}&dropoff[longitude]={}&dropoff[nickname]=Your%20Friend&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d".format(self.latitude, self.longitude)
 
     def to_json(self):
         return {
