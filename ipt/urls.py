7ccb88452f5f2ea4524a0edc1bea569af062a1c0
Jennifer Blight <jgblight@gmail.com>
2016-05-15 09:48:27 -0700

    template

--- ipt/urls.py
+++ ipt/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^login/?$', views.begin_auth, name="twitter_login"),
     url(r'^logout/?$', views.logout, name="twitter_logout"),
     url(r'^thanks/?$', views.thanks, name="twitter_callback"),
-    url(r'^r/?$', views.request_button, name="request_button"),
+    url(r'^r/(?P<id>[0-9]*)/?$', views.request_button, name="request_button"),
     url(r'^users/?$', views.UserSearchView.as_view(), name="user_search")
 ]
 
