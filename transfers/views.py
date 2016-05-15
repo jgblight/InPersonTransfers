79953702a780d268821cb2c6710b60086eb4388c
Jennifer Blight <jgblight@gmail.com>
2016-05-14 19:38:55 -0700

    json

--- transfers/views.py
+++ transfers/views.py
@@ -105,4 +105,4 @@ class PaymentRequestViewSet(viewsets.ViewSet):
         return Response(PaymentRequest.objects.get(id=pk).to_json)
 
     def create(self, request):
-        return Response(PaymentRequest.from_json(**data))
+        return Response(PaymentRequest.from_json(**request.data))
